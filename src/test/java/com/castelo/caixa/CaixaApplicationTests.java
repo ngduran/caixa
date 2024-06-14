a3a140f30b9bf09c2e528f45a3f0aaece3557dc5
ngduran <norbertogarciaduran@gmail.com>
2024-06-13 22:24:51 -0300

    Arquivos iniciais do projeto spring-boot

---
+++ src/test/java/com/castelo/caixa/CaixaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castelo.caixa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaixaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
